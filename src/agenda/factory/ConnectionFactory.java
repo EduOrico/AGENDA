54f8225230e14af0ff92c5943a2a447b93ebaf62
Eduardo Florêncio Orico Barros <oricoeduardo@gmail.com>
2022-10-13 18:46:21 -0300

    Criação do Projeto

---
+++ src/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package agenda.factory;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+
+}
+
+//senha do banco
+//caminho de dados com porta de dado9s 
+private static final String USERNAME = "root";
+
+private static final String DATABASE_URL =
+        "jdbc:mysql://localhost:3306/agenda";
+
+
+
